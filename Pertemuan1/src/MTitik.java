f774c6890b63f46c2938aa0fb5a88ae4616212d8
Dian Aulya <mimoaulya966@gmail.com>
2026-03-05 15:42:52 +0700

    Nyusul part 2

--- Pertemuan1/src/MTitik.java
+++ Pertemuan1/src/MTitik.java
@@ -5,5 +5,12 @@
  */
 
 public class MTitik {
-
+    public static void main(String[] args){
+        Titik T1 = new Titik(3, 4);
+        T1.setAbsis(3);
+        T1.setOrdinat(4);
+        T1.printTitik();
+        T1.geser(3,4);
+        T1.printTitik();
+    }
 }
